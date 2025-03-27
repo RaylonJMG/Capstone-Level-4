b659d8992148f90480d7207247934e8550b66536
RaylonJMG <ryj2213@gmail.com>
2025-03-27 18:40:00 -0500

    created front end folder and added corresponding files

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -1,6 +1,6 @@
 export default {
 	presets: [
-		"@babel/preset-env", //importing export syntax
+		"@babel/preset-env", //import/export old syntax for Jest to work properly
 		"@babel/preset-react", //converting jsx into js
 		"@babel/preset-typescript", //compiles ts into js
 	],
