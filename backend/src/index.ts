9889660ffa37fb5219adcca120f609444e6abfaa
RaylonJMG <ryj2213@gmail.com>
2025-03-27 19:43:08 -0500

    added Communication branch

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import { root } from "./routes/root";
 import { api } from "./routes/api";
+import { home } from "./routes/home";
 
 const hostname = "localhost";
 const port = 8000;
@@ -11,6 +12,7 @@ const app = express();
 app.use(cors());
 app.get("/", root);
 app.get("/api", api);
+app.get("/home", home);
 app.listen(port, hostname, handleListen);
 
 function handleListen() {
