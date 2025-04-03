71f7e71e85a8c971a21890208760b7227fe309a8
RaylonJMG <ryj2213@gmail.com>
2025-04-03 00:47:05 -0500

    added TS declaration file

---
+++ frontend/dist/main.js
@@ -0,0 +1,25 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.tsx":
+/*!***********************!*\
+  !*** ./src/index.tsx ***!
+  \***********************/
+/***/ (() => {
+
+throw new Error("Module build failed (from ./node_modules/.pnpm/babel-loader@9.2.1_@babel+core@7.26.10_webpack@5.98.0/node_modules/babel-loader/lib/index.js):\nError: ENOENT: no such file or directory, open 'C:\\Users\\ryj22\\Capstone Level 4\\frontend\\src\\index.tsx'");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.tsx"]();
+/******/ 	
+/******/ })()
+;
+//# sourceMappingURL=main.js.map
