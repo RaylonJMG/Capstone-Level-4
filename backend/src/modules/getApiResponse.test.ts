2d450908c40efc6177c49f6c4b8816f5c98f88d8
RaylonJMG <ryj2213@gmail.com>
2025-04-14 03:35:33 -0500

    added tests/functions to check API response

--- backend/src/modules/getApiResponse.test.ts
+++ backend/src/modules/getApiResponse.test.ts
@@ -1,21 +1,35 @@
-import { response } from "express";
+import { request, response } from "express";
 import { getApiResponse } from "./getApiResponse";
 
 describe(getApiResponse, () => {
-	it("returns a response from the API", () => {
+	it("returns a response from the Advice API", async () => {
 		//ARRANGE
-
+		const apiResponse = {
+			slip: {
+				id: "",
+				advice: "This is an example advice response.",
+			},
+		};
 		//ACT
-		const result = axios.get(response);
+		const result = await getApiResponse(request, response);
 
 		//ASSERT
+		expect(result).toBeDefined();
+		expect(result).toHaveProperty("slip");
+		expect(result).toHaveProperty("id");
 	});
-	it("returns a response from the API", () => {
+	it("does not return a response from the Advice API when there is no request sent to the API server", async () => {
 		//ARRANGE
-
+		const apiResponse = {
+			slip: {
+				id: "",
+				advice: "This is an example advice response.",
+			},
+		};
 		//ACT
-		const result = axios.get(response);
+		const result = await getApiResponse(undefined, undefined);
 
 		//ASSERT
+		expect(result).toBeUndefined();
 	});
 });
--- backend/src/modules/getApiResponse.ts
+++ backend/src/modules/getApiResponse.ts
@@ -1,3 +1,9 @@
+import axios from "axios";
 import { Request, Response } from "express";
 
-export function getApiResponse(request: Request, response: Response) {}
+export async function getApiResponse(request: Request, response: Response) {
+	const apiResponse = await axios.get("https://api.adviceslip.com/advice");
+	const result = apiResponse.data;
+	const advice = result.slip.advice;
+	response.send(advice);
+}
