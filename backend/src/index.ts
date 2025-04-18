cd44111713a5a7fcd03fb1654513f40d3c372f83
RaylonJMG <ryj2213@gmail.com>
2025-04-17 22:20:59 -0500

    path and route handler for API function

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,9 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import { root } from "./routes/root";
-import { api } from "./routes/api";
 import { home } from "./routes/home";
 import { auth } from "./routes/auth";
+import { api_advice } from "./routes/api_advice";
 
 const hostname = "localhost";
 const port = 8000;
@@ -12,7 +12,7 @@ const path = "/";
 const app = express();
 app.use(cors());
 app.get("/", root);
-app.get("/api", api);
+app.get("/api_advice", api_advice);
 app.get("/home", home);
 app.get("/auth", auth);
 app.listen(port, hostname, handleListen);
--- backend/src/routes/api_advice.ts
+++ backend/src/routes/api_advice.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 import { getApiResponse } from "../modules/getApiResponse";
 
-export function api(request: Request, response: Response) {
+export function api_advice(request: Request, response: Response) {
 	const apiResponse = getApiResponse();
 	response.send(apiResponse);
 }
