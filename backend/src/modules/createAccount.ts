6d013877b07e289b600f851771773527af0f18c4
RaylonJMG <ryj2213@gmail.com>
2025-04-17 21:22:41 -0500

    modified the createAccount function

--- backend/src/modules/createAccount.ts
+++ backend/src/modules/createAccount.ts
@@ -1,3 +1,22 @@
-export function createAccount(){
-    
+import { DynamoDB } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocument } from "@aws-sdk/lib-dynamodb";
+
+export async function createAccount() {
+	const apiKey = {
+		region: process.env.region,
+		credentials: {
+			accessKeyId: process.env.accessKeyId,
+			secretAccessKey: process.env.secretAccessKey,
+		},
+	};
+	const client = new DynamoDB(apiKey);
+	const niceClient = DynamoDBDocument.from(client);
+
+	const request = {
+		TableName: "logins",
+		Item: { email: "newEmail@logins.com", password: "newPassword" },
+	};
+
+	const response = await niceClient.put(request);
+	return response;
 }
