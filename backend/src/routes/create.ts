e0784ad7e91746e7754010cc8284bb6ff56badb6
RaylonJMG <ryj2213@gmail.com>
2025-04-18 00:11:50 -0500

    saved changes to create handler

--- backend/src/routes/create.ts
+++ backend/src/routes/create.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import { createAccount } from "../modules/createAccount";
 
 export async function create(request: Request, response: Response) {
 	const result = await createAccount();
